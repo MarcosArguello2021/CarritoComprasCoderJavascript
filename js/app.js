a9e79fc8522d0298db1799dbb95c40dc1e530d0c
Marcos Argüello <arguello.marcos@protonmail.ch>
2022-04-21 22:51:06 -0300

    Se incorporó eventos para interactuar con el carrito

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //Objeto para modelar el catalogo
-
 class Producto {
   ///Declaro la funcion constructora
   constructor(nombre, precio, id, img) {
@@ -32,46 +31,132 @@ productos.push(new Producto("Nicasia blanc ",1350,5,"/img/catalogo/Nicasia_blanc
 productos.push(new Producto("Salentein rose",800,6,"/img/catalogo/Salentein_rpse.jpg"));
 console.dir(productos);
 
-/////DOM
+
 // Recorremos el array productos y se crea una card con las propiedades de cada objeto.
-for (const producto of productos) {
-  let cardNueva = document.createElement("div"); // Creo un elemento nuevo.
-  cardNueva.innerHTML = ` 
-  <img src= ${producto.img} class="card-img-top" alt="...">
-  <h5 class="card-title">${producto.nombre}</h5>
-  <div class="card-body text-center">
-   
-    <p class="card-text">$ ${producto.precio}</p>
-    <button id="${producto.id}" class="  third">Añadir al carrito   <i class="fa fa-shopping-cart in-card" aria-hidden="true"></i> </button>
-  </div>
-  `; 
+//Agregar al carrito:
+const cartContainer = document.querySelector("#cart");
+const contadorCarrito = document.querySelector("#cart-counter");
 
-  document.getElementById("cards").append(cardNueva); //"pusheo" ese elemento a un div con id="cards", que se encuentra en mi index.html.
 
-  cardNueva.classList.add("card", "col-4", "m-3"); //Le asigno una clase a ese nuevo div para darle estilos.
+function agregarProducto(e) {
+  e.target.setAttribute("disabled", true); // Desactivo el Boton.
+  e.target.innerHTML = "Agregado"; // Cambio el Texto
+  e.target.style.minWidth = "200px";
+  let productoClickeado = productos.find((item) => item.id == e.target.id); // Encontramos el producto dentro del array
+  let arrayLocal = JSON.parse(localStorage.getItem("Carrito"));
+  arrayLocal.push(productoClickeado);
+  localStorage.setItem("Carrito", JSON.stringify(arrayLocal)); // Convierto el array a string y lo envio al local storage.
+  actualizarCarrito(arrayLocal);
+  actualizarBotones(arrayLocal);
 }
 
-///Eventos
-const openBtn = document.getElementById("carrito");
-const sideBarWrapper = document.querySelector("div.side-bar-wrapper");
+function actualizarCarrito(local) {
+  cartContainer.innerHTML = ""; //Limpio el contenedor, para no dublicar cards.
 
-openBtn.addEventListener("click", function () {
-  sideBarWrapper.style.width = "100vw";
+  for (const productoAgregado of local) {
+    cartContainer.innerHTML += ` <div class="d-flex justify-content-between align-items-center mt-3 p-4 items rounded border-bottom border-top" >
+    <img class="rounded border" src=${productoAgregado.img} width="100">
+        <div class="ml-2"><span class="font-weight-bold d-block m-2">${productoAgregado.nombre}</span><span class="d-block ml-5 font-weight-bold">$${productoAgregado.precio}</span></div>
+        <div class="ml-2 rounded"> <span><i class="fa fa-minus " aria-hidden="true"></i> </span> <span class ="item-count rounded">1</span>  <span><i class="fa fa-plus rounded" aria-hidden="true"></i> </span>  </div>
+        <span class="eliminar-item" id= "#${productoAgregado.id}"><i class="fa fa-trash" aria-hidden="true"></i> </span>
+</div>`;
+  }
+  let btnsEliminar = document.querySelectorAll(".eliminar-item");
 
-  document.querySelector("#side-bar").style.width = "400px";
-});
+  for (const btn of btnsEliminar) {
+    btn.addEventListener("click", eliminarProducto);
+  }
 
-const closeBtn = document.querySelector("#close-cart");
+  contadorCarrito.innerText = local.length; /// Actualizamos el contador del carrito
 
-closeBtn.addEventListener("click", function () {
-  sideBarWrapper.style.width = "0vw";
-  document.querySelector("#side-bar").style.width = "0px";
-});
+  document.querySelector("#total").innerText = sumarTotal(local);
+}
+function actualizarBotones(local) {
+  const btns = document.querySelectorAll("button.third");
 
-sideBarWrapper.addEventListener("click", function (e) {
-  sideBarWrapper.style.width = "0vw";
-  document.querySelector("#side-bar").style.width = "0px";
-});
-document.querySelector("#side-bar").addEventListener("click", function (e) {
-  e.stopPropagation();
-});
+  for (const item of local) {
+    for (const btn of btns) {
+      if (item.id == btn.id) {
+        let botonAdd = document.getElementById(`${item.id}`);
+        botonAdd.setAttribute("disabled", true); // Desactivo el Boton.
+        botonAdd.innerHTML = "Agregado"; // Cambio el Texto
+        botonAdd.style.minWidth = "202px"; //Eso lo hago aca por que me dio fiaca modificarlo del CSS y ademas como ejemplo de modificar el CSS con eventos.
+      }
+    }
+  }
+}
+function eliminarProducto() {
+  let arrayLocal = JSON.parse(localStorage.getItem("Carrito"));
+  let productoEliminado = arrayLocal.find((item) => `#${item.id}` === this.id);
+  let productId = productoEliminado.id;
+  let btnProducto = document.getElementById(productId);
+  document.getElementById(productId).innerHTML = "agregar al carrito";
+
+  btnProducto.disabled = false;
+  // Aca podriamos utilizar un filter(), para sacar el producto, tal vez seria mas prolijo, pero para que vean mas metodos.
+  let indiceDeProducto = arrayLocal.indexOf(productoEliminado); ///indexOf devuelve el indice del elemento en el array.
+
+  arrayLocal.splice(indiceDeProducto, 1); // splice() elimina elementos, requiere dos parametros. El primero es el indice, y el segundo la cantidad de elementos que queres eliminar.
+
+  localStorage.setItem("Carrito", JSON.stringify(arrayLocal)); // Actualizamos el local.
+
+  actualizarCarrito(arrayLocal);
+}
+
+//SUMAR TOTAL
+function sumarTotal(cart) {
+  let total = cart.reduce((acc, item) => acc + item.precio, 0);
+  return total;
+}
+
+function crearCards(productos) {
+  for (const producto of productos) {
+    let cardNueva = document.createElement("div"); // Creo un elemento nuevo.
+    cardNueva.innerHTML = ` 
+  <img src= ${producto.img} class="card-img-top border" alt="...">
+  <h5 class="card-title">${producto.nombre}</h5>
+  <div class="card-body text-center">
+    <p class="card-text">$ ${producto.precio}</p>
+    <button id="${producto.id}" class="third add">Añadir al carrito<i class="fa fa-shopping-cart in-card" aria-hidden="true"></i> </button>
+  </div>
+    `; //Le asigno el html mediante plantilla literal. **IMPORTANTE usar BACKTICKS (``)en vez de comillas regulares** .
+
+    document.getElementById("cards").append(cardNueva); //"pusheo" ese elemento a un div con id="cards", que se encuentra en mi index.html.
+
+    cardNueva.classList.add("card", "col-4", "m-3"); //Le asigno una clase a ese nuevo div para darle estilos.
+  }
+
+  //Agrego los eventos Click
+  const btns = document.querySelectorAll(".add");
+  for (const boton of btns) {
+    boton.addEventListener("click", agregarProducto);
+  }
+}
+
+ ///Carga de la app:
+
+crearCards(productos);
+
+let carritoDelLocal = JSON.parse(localStorage.getItem("Carrito")); //Realizo la llamada al local.
+
+console.log(carritoDelLocal);
+
+if (carritoDelLocal) {
+  actualizarBotones(carritoDelLocal);
+
+  actualizarCarrito(carritoDelLocal);
+} else {
+  localStorage.setItem("Carrito", "[]");
+}
+
+document.querySelector("#clear").addEventListener("click", eliminarCarrito);
+
+function eliminarCarrito() {
+  document.querySelector("#cart").innerHTML = "";
+  localStorage.setItem("Carrito", "[]");
+  document.querySelector("#cards").innerHTML = "";
+  crearCards(productos);
+  document.querySelector("#cart-counter").innerText = "0";
+  document.querySelector("#total").innerText = "0";
+}
+
